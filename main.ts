3dab76d6dc57f8bc14335664f41b1c1a7bfd0059
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2024-04-06 19:43:51 -0700

    get a square

--- main.ts
+++ main.ts
@@ -1,7 +1,36 @@
-function addNumbers(a: number, b: number) { 
-  return a + b; 
-} 
+class Breakout {
+  canvas: HTMLCanvasElement;
+  context: CanvasRenderingContext2D;
+  constructor() {
+    this.canvas = document.createElement("canvas") as HTMLCanvasElement;
+    document.body.appendChild(this.canvas)
+    this.context =this.canvas.getContext("2d") as CanvasRenderingContext2D;
+    console.log(this.context);
+    this.canvas.width=innerWidth;
+    this.canvas.height=innerHeight;
+    document.addEventListener("keydown", this.draw.bind(this));
+    document.addEventListener("keyup", this.draw.bind(this));
+    requestAnimationFrame(this.draw.bind(this));
+  }
+  private keyreleased(e:KeyboardEvent){
+console.log("DOWN",e);
+  }
+  private keypressed(e:KeyboardEvent){
+    console.log("UP",e);
+  }
+  private draw(){
+    console.log(this);
+    console.log("HERE")
+    const ctx:CanvasRenderingContext2D = this.context;
 
-var sum: number = addNumbers(10, 15) 
+    console.log(ctx);
+    ctx.fillRect(50,50,50,50)
+    requestAnimationFrame(this.draw.bind(this));
+  }
+}
 
-console.log('Sum of the two numbers is: ' +sum);
+
+
+onload=function(){
+  new Breakout();
+}
